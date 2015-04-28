772af04213c10722d7bea2f538cee3e8684b5b12
ulfayuuki <ulfagustiani51@gmail.com>
2015-04-27 23:34:21 -0700

    Commit Pertama, Versi 1

---
+++ Desktop/projek/index.php
@@ -0,0 +1,18 @@
+<html>
+<title>Nilai Akademik_Ulfa</title>
+<link href="gaya.css" rel="stylesheet" type="text/css">
+<body>
+<div id="bungkus"><br>
+<h1 align="right">Aplikasi Web Nilai Siswa</h1>
+	<div id="frame">
+	<form name="login" action="prosses.php?aksi=login" method="POST">
+		<table>
+			<tr><th>Username</th><th> : <input type="text" name="username"></th></tr>
+			<tr><th>Password</th><th> : <input type="password" name="password"></th></tr>
+			<tr><th>&nbsp;</th><th align="left">&nbsp;&nbsp;&nbsp;<input type="submit" value="&nbsp;" class="btn" width="63" height="25"></th></tr>
+		</table>
+	</form>
+    </div>
+</div>
+</body>
+</html>
